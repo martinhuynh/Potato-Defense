c22585b95f430ecfde97b88ddff722f33d746a51
martinhuynh <martinhuynhp@gmail.com>
2022-10-30 18:48:15 -0700

    Added plowing by hitting j. Changes the tile the player is standing on with dirt.

---
+++ Potato-Defense/Assets/Scripts/TileMapManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapManager : MonoBehaviour
+{
+    [SerializeField]
+    public static TileMapManager tileMapManager;
+    public Tilemap tilemap;
+
+    public TileBase dirt;
+
+    public TileMapManager()
+    {
+        if (tileMapManager == null) tileMapManager = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void setDirt(Vector3 position)
+    {
+        Debug.Log(position);
+        tilemap.SetTile(new Vector3Int((int)Mathf.Round(position.x), (int)Mathf.Round(position.y), (int)Mathf.Round(position.z)), dirt);
+    }
+}

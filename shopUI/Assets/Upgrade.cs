bd8b3c01cd86238e9e024dd63fefccaef523fd72
circleSquare2019 <56340878+circleSquare2019@users.noreply.github.com>
2022-10-31 17:06:24 -0700

    Add files via upload

--- shopUI/Assets/Upgrade.cs
+++ shopUI/Assets/Upgrade.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Upgrade : MonoBehaviour
 {
+    private GameObject upgrade;
+    private int upgradeTimes=0;
+    private int addHeath =100;
+    private int addLevel =1;
     // Start is called before the first frame update
     void Start()
     {

ca2433ba2b99d6b81664df9c17a9a5f471c7a88d
capta1nLz <yuzewang0706@gmail.com>
2022-11-29 20:51:15 -0800

    update instruction, and wave UI
    move instruction to pause menu, make assets for the keys, switching fonts, and adjusting wave UI to a horizontal bar instead of a square-ish box

--- Potato-Defense/Assets/Scripts/Systems/WaveSystem/WavesLeft.cs
+++ Potato-Defense/Assets/Scripts/Systems/WaveSystem/WavesLeft.cs
@@ -8,21 +8,25 @@ public class WavesLeft : MonoBehaviour
     WaveSystem waveSystem;
     TMP_Text textMP;
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
         waveSystem = GameObject.Find("WaveSystem").GetComponent<WaveSystem>();
         textMP = gameObject.GetComponentInChildren<TMP_Text>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void FixedUpdate()
     {
-        textMP.text = "Waves Left: " + waveSystem.getWavesLeft();
+        int currentWave = 6 - waveSystem.getWavesLeft();
+        textMP.text = "Waves: " + currentWave + "/5";
     }
 }

452a102b7f63057cd362de10d4c5baf41c811c8d
capta1nLz <yuzewang0706@gmail.com>
2022-12-06 16:30:08 -0800

    add attacking enemy sound

--- Potato-Defense/Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Potato-Defense/Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
+[RequireComponent(typeof(AudioSource))]
 public class EnemyBehavior : MonoBehaviour
 {
     [SerializeField]
@@ -9,12 +11,27 @@ public class EnemyBehavior : MonoBehaviour
     private float health;
     private HealthbarBehavior hb;
 
+    public AudioSource hittingSoundEffect;
+    public AudioSource dieSoundEffect;
+
+    /*    public AudioClip hittingSoundEffect;
+        public AudioClip dieSoundEffect;
+        public AudioSource audio;*/
+
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
         hb = this.GetComponentInChildren<HealthbarBehavior>();
         hb.UpdateHealthBar(health, maxHealth);
+
+        /*hittingSoundEffect = gameObject.AddComponent<AudioSource>();
+        dieSoundEffect  = gameObject.AddComponent<AudioSource>();*/
+
+/*        AudioSource[] audioSources = GetComponents<AudioSource>();
+        audio = audioSources[0];
+        hittingSoundEffect = audioSources[0].clip;
+        dieSoundEffect = audioSources[1].clip;*/
     }
 
     // Update is called once per frame
@@ -33,21 +50,42 @@ public class EnemyBehavior : MonoBehaviour
     {
         health -= damage;
         hb.UpdateHealthBar(health, maxHealth);
+
+        // Debug.Log("damage taken");
+        // hittingSoundEffect.Play();
+        if (health > 0)
+        {
+            hittingSoundEffect.Play();
+            //audio.PlayOneShot(hittingSoundEffect, 0.1F);
+        }
+        else
+        {
+            dieSoundEffect.Play();
+            //audio.PlayOneShot(dieSoundEffect, 0.1F);
+        }
+        
         if (health <= 0)
         {
+            //dieSoundEffect.Play();
+            
             Die();
+            
             return true;
         }
         return false;
+
+
     }
 
     public void Die()
     {
+       
         if (gameObject.activeSelf)
-        {
+        {           
             gameObject.SetActive(false);
+            
             Destroy(gameObject);
-        }
+        }     
     }
 
     // Getters
